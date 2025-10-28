2f5c5bbc6ca9e09dcfc2c919688c397787709864
MachNP <essilor.itq@gmail.com>
2025-10-28 11:33:37 +0100

    Mise en ligne du cours suivi sur les bases de JS

---
+++ file-test.js
@@ -0,0 +1,17 @@
+let variable = "hello";
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <script>
+//         alert ("Ceci est un test");
+//     </script>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+//     <h1>Bonjour tout le monde</h1>
+    
+// </body>
+// </html>
---
+++ file.js
@@ -0,0 +1,5 @@
+alert ("write down") ;
+
+var unTexte = "voici un texte";
+// const = constante donnee qui ne change jamais
+const prenom = "Justine" ;
